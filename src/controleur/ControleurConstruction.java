17159c45011632802f981da100766d15b5fc12c0
AlexandreGARCES <alexandre.garces@hotmail.fr>
2022-05-13 23:27:18 +0200

    modification de l'interface + support pour la recherche

--- src/controleur/ControleurConstruction.java
+++ src/controleur/ControleurConstruction.java
@@ -4,6 +4,8 @@ import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -17,6 +19,8 @@ import javafx.scene.SubScene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.ScrollPane;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import vue.Gestion3D;
@@ -60,16 +64,79 @@ public class ControleurConstruction implements Initializable {
 
     @FXML
     private Button boutonUndo;
+    
+    @FXML
+    private Button boutonRechercherBloc;
+
+    @FXML
+    private Button boutonRechercherConstruction;
 
     @FXML
     private SubScene subScene3D;
     
     
+    @FXML
+    private CheckBox boxBlancBloc;
+
+    @FXML
+    private CheckBox boxBleuBloc;
+
+    @FXML
+    private CheckBox boxCarréBloc;
+
+    @FXML
+    private CheckBox boxCyanBloc;
+
+    @FXML
+    private CheckBox boxGrandBloc;
+
+    @FXML
+    private CheckBox boxGrisBloc;
+
+    @FXML
+    private CheckBox boxJauneBloc;
+
+    @FXML
+    private CheckBox boxMagentaBloc;
+
+    @FXML
+    private CheckBox boxMoyenBloc;
+
+    @FXML
+    private CheckBox boxNoirBloc;
+
+    @FXML
+    private CheckBox boxPetitBloc;
+
+    @FXML
+    private CheckBox boxRectBloc;
+
+    @FXML
+    private CheckBox boxRondBloc;
+
+    @FXML
+    private CheckBox boxRougeBloc;
+    
+    
+    @FXML //oubliez pas de lui mettre truc que vous voulez ( a la place du string )
+    private ListView<String> listeResultatRechercheBloc;
+    
+    
     @Override
 	public void initialize(URL arg0, ResourceBundle arg1) {
-		
-		
-		
+		//Permet d'afficher la recherche
+    	//listeResultatRechercheBloc.getItems().addAll(*une collection de bloc pour la recherche*);
+    	//
+    	//Listener pour obtenir le bloc sur lequel vous avez clicker
+    	//listeResultatRechercheBloc.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<String>() {
+    	
+    		//la méthode correspondante.
+			//@Override
+			//public void changed(ObservableValue<? extends String> arg0, String arg1, String arg2) {
+    		//
+			//}
+    		
+    	//});
 		
 	}
     
@@ -110,7 +177,28 @@ public class ControleurConstruction implements Initializable {
     
     @FXML
     void SauvegarderConstruction(ActionEvent event) {
+    	// la methode qui va appelé sauvegarder du Modèle
+    }
+    
+    @FXML
+    void rechercheMultiCritBloc(ActionEvent event) {
+    	//Pour savoir si boxRougeBloc est activée:
+    	//if(boxRougeBloc.isSelected()){ ---> renvoie un booléen
+    	//
+    	//}
+    	//(Pareil pour les autres box bien sur)
+    	//
+    	//Si tu veux pas avoir plus box selectionnées en même temps:
+    	//if(boxRougeBloc.isSelected()){
+    	//	boxBleuBloc.setSelected(false);  ( déselectionne la checkBox )
+    	//}
+    	//Apres si il vous manque des choses youtube est ton meilleur amis car je vois pas d'autre méthodes utiles.
     	
+    	
+    	
+    	
+    	//Et juste pour morti car martin t'as pas le pb, la scroll pane te permet de retrecir le bouzin sans enlever l'interface tu peux ( en scrollant ) acceder aux boutons qui te posaient pb. 
+    	//Bien sur la Subscene bug donc faudra faire avec
     }
 
 }
