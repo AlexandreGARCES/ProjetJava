7620c54f1ba4805172b38505f661beb188c246c5
AlexandreGARCES <alexandre.garces@hotmail.fr>
2022-05-16 16:24:14 +0200

    Ajout PopUp pour savoir la couleur de terrain voulu

--- src/controleur/ControleurMenu.java
+++ src/controleur/ControleurMenu.java
@@ -1,12 +1,15 @@
 package controleur;
 
 import java.io.IOException;
+import java.util.Optional;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.Node;
@@ -38,8 +41,17 @@ public class ControleurMenu extends Controleur{
 
     @FXML
     void switchFXMLConstruction(ActionEvent event) throws IOException {
-    	this.mod.raz();
-    	this.changerFenetre("Construction", event);
+    	String couleurPlateau = null;
+    	TextInputDialog text = new TextInputDialog();
+    	text.getDialogPane().setContentText("Quelle couleur de terrain voulez-vous choisir ?");
+    	Optional<String> resultat = text.showAndWait();
+    	TextField input = text.getEditor();
+    	if (input.getText() != null && input.getText().toString().length() != 0) {
+    		couleurPlateau = input.getText().toString();
+    		System.out.println(couleurPlateau);
+    		this.mod.raz();
+        	this.changerFenetre("Construction", event);
+    	} else { System.out.println("entrez une couleur !"); }
     }
     
     
